15b88a45694b466c5d1a0fa9ba3ad29a52f28869
Daniel Mattox <dmattox10@gmail.com>
2020-02-28 11:14:27 -0500

    Added auth-router endpoints

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,14 +2,60 @@ const bcrypt = require('bcrypt')
 const router = require('express').Router()
 const jwt = require('jsonwebtoken')
 const Users = require('../models/users')
-const auth = require('../middleware/auth')
+const auth = require('./authenticate-middleware')
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body
+
+  const hash = bcrypt.hashSync(user.password, 8)
+
+  user.password = hash
+
+  Users.add(user)
+    .then(saved => {
+      // create token here
+      res.status(201).json(saved)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 })
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body
+    
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // create token here
+        const token = generateToken(user)
+        res.status(200).json({ 
+            message: `Welcome ${user.username}!`, 
+            token 
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 })
+
+function generateToken(user) {
+  const payload = {
+      username: user.username
+  }
+  const secret = process.env.JWT_SECRET || 'is it secret, is it safe?'
+
+  const options = {
+      expiresIn: '1h'
+  }
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router
